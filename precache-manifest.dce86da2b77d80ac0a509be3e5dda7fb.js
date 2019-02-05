637736104bbbbaef00b28dcf91aa67b677cac666
Neto6391 <neto6391@gmail.com>
2019-02-05 19:11:51 -0300

    Updates

---
+++ precache-manifest.dce86da2b77d80ac0a509be3e5dda7fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e8868cc8250b114ba8dd960b85bbd53",
+    "url": "/website/static/media/spinner.4e8868cc.gif"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "dbabe3a87467b6ed9ec5",
+    "url": "/website/static/js/main.dbabe3a8.chunk.js"
+  },
+  {
+    "revision": "0d78f161fb750d45a4a2",
+    "url": "/website/static/js/1.0d78f161.chunk.js"
+  },
+  {
+    "revision": "dbabe3a87467b6ed9ec5",
+    "url": "/website/static/css/main.5e02f405.chunk.css"
+  },
+  {
+    "revision": "ef00474ba97ec319f57c345ca4697523",
+    "url": "/website/index.html"
+  }
+];
